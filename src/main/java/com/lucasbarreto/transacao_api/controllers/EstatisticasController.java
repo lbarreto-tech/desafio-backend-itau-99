a641fb22e436e67723c08195cac1e7d71c3e8de0
lbarreto-tech <lucaslbcs2003@gmail.com>
2025-02-10 15:23:13 -0300

    @GetMapping

--- src/main/java/com/lucasbarreto/transacao_api/controllers/EstatisticasController.java
+++ src/main/java/com/lucasbarreto/transacao_api/controllers/EstatisticasController.java
@@ -2,6 +2,7 @@ package com.lucasbarreto.transacao_api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class EstatisticasController {
 	private EstatisticasService estatisticasService;
 	
 	
+	@GetMapping
 	public ResponseEntity<EstatisticasDTO> buscarEstatisticas(@RequestParam(value = "intervaloBusca", required = false, defaultValue = "60") int intervaloBusca){
 		return ResponseEntity.ok(estatisticasService.calcularEstatisticasTransacoes(intervaloBusca));
 	} 
